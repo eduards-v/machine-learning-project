e4f675f45b9aa3a0f07b7813e2417f39517f932b
eduards-v <eduardsvagalsgti@gmail.com>
2017-11-15 14:51:48 +0000

    Added upload file component | bug: sending wrong base64 padding | Changed to official react-bootstrap

--- Client/my-app/src/components/UploadImage.js
+++ Client/my-app/src/components/UploadImage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import FileBase64 from 'react-file-base64';
 
 import './UploadImage.css';
-import { Button } from 'reactstrap';
+import { Button, Grid, Row, Col, Form} from 'react-bootstrap';
 
 class UploadImage extends Component{
 
@@ -10,29 +11,76 @@ class UploadImage extends Component{
 
         // component state can be stored here
         this.state = {
-            hello: 'Hello, World!'
+            hello: 'Hello, World!',
+            filename: '',
+            base64_image: ''
         }
 
         this.updateHello = this.updateHello.bind(this);
+        this.handleFile = this.handleFile.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     updateHello(newMessage){
         this.setState({ hello: newMessage.target.value});
     }
+
+    handleFile = (file) => {
+        Object.keys(file).map((key, index) =>
+        {
+            if(key === 'name'){
+                this.setState({ filename: file[key] });
+            }
+            if(key === 'base64'){
+
+                this.setState({ base64_image: file[key] });
+            }
+        })
+    }
+
+    handleSubmit = (event) => {
+                fetch('http://127.0.0.1:5000/upload_image', {
+                    //mode: 'no-cors'
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        filename: this.state.filename, 
+                        image: this.state.base64_image.replace('data:image/jpeg;base64,', String.prototype)
+                    })
+                })
+                event.preventDefault();
+            }
+
+
     render(){
         return(
-            <div className = 'p-2 ml-5 mr-5 justify-content-center Body-style'>
-                <div className = 'row'>
-                    <div className = 'col-sm'>
+
+            <Grid className = 'p-4 ml-5 mr-5 body-style' >
+                <Row className = 'justify-content-center'>
+                    <Col>
                         <input type = "text" value = {this.state.hello} 
                                onChange = {this.updateHello}/>
-                    </div>
-                    <div className = 'col-sm'>
-                        <p>{this.state.hello}</p>
-                    </div>
-                </div>
 
-            </div>
+                        <p>{this.state.hello}</p>
+                    </Col>
+                    
+                </Row>
+
+                <Row className = 'justify-content-center'>
+                    <Col>
+                        <Form inline onSubmit = {this.handleSubmit} >             
+                            <FileBase64 onDone = {this.handleFile.bind(this)}/>
+                            <Button bsStyle = 'danger' type = "submit" >Send image</Button>
+                        </Form>
+
+                       <textarea value = {this.state.filename} />
+                    </Col>
+                </Row>
+
+            </Grid>
         );
     }
 }
